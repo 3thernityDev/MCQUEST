a39ab136022e7fd154df3118a4c6ac169f37c19d
3thernitydev <3thernitydev@gmail.com>
2025-05-21 08:44:49 +0400

    [INIT] POROJECT

---
+++ src/main/java/re/ethernitydev/mcquest/McquestApplication.java
@@ -0,0 +1,13 @@
+package re.ethernitydev.mcquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McquestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McquestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/re/ethernitydev/mcquest/McquestApplicationTests.java
@@ -0,0 +1,13 @@
+package re.ethernitydev.mcquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McquestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
